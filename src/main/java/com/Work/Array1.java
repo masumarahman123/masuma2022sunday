0e5fb13879a8d27e7a55d2c19bebe1cabd8bb2a1
masumarahman123 <masumarahman123@yahoo.com>
2022-07-30 18:36:09 -0400

    add Array

---
+++ src/main/java/com/Work/Array1.java
@@ -0,0 +1,25 @@
+package com.Work;
+
+public class Array1 {
+
+	public static void main(String[] args) {
+		//static
+		
+		int a[] =new int [4];
+		
+		for(int i=0;i<a.length;i++) {
+			System.out.println(a[i]);
+		}
+
+		
+		//dynamic
+		
+		int a1[]= {2,4,6,8,9,10};
+		
+		for(int i=0;i<a1.length;i++) {				
+		
+		System.out.println(a1[i]);}
+		
+	}
+
+}
