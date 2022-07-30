c1d62d362e8a619be49d98e471911d9f6a593c10
masumarahman123 <masumarahman123@yahoo.com>
2022-07-30 18:47:26 -0400

    2nd update

--- src/main/java/com/Work/Array1.java
+++ src/main/java/com/Work/Array1.java
@@ -4,7 +4,7 @@ public class Array1 {
 
 	public static void main(String[] args) {
 		//static
-		System.out.println("The static array is  :");
+		System.out.println("The static array is ###### :");
 		int a[] =new int [4];
 		
 		for(int i=0;i<a.length;i++) {
@@ -14,7 +14,7 @@ public class Array1 {
 		
 		//dynamic
 		
-		System.out.println("The dynamic array is  :");
+		System.out.println("The dynamic array is *********** :");
 		int a1[]= {2,4,6,8,9,10};
 		
 		for(int i=0;i<a1.length;i++) {				
