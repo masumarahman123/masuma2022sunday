05fb2adb46cefb8fa6811327324ba4f192a67d90
masumarahman123 <masumarahman123@yahoo.com>
2022-08-18 13:05:57 -0400

    selenium_one

--- src/main/java/com/Selenium/Selenium_one.java
+++ src/main/java/com/Selenium/Selenium_one.java
@@ -15,6 +15,8 @@ import org.testng.Assert;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Selenium_one {
+	
+	//
 //...//input[@value='radio2']..//radio button index follow kortesa
 	////input[@id='autocomplete']...hello
 	////select[@id='dropdown-class-example']..select
