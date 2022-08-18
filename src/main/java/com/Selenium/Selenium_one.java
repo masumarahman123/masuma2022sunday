e99be0be6418e724ebc0c76d5e16d66c003cebec
masumarahman123 <masumarahman123@yahoo.com>
2022-08-18 13:25:54 -0400

    selel

--- src/main/java/com/Selenium/Selenium_one.java
+++ src/main/java/com/Selenium/Selenium_one.java
@@ -16,7 +16,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class Selenium_one {
 	
-	//
+	
 //...//input[@value='radio2']..//radio button index follow kortesa
 	////input[@id='autocomplete']...hello
 	////select[@id='dropdown-class-example']..select
